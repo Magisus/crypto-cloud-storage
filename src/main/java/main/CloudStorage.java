a43fd2cdb8d76e1d2ee0c705dcf0e3cd6bd67a69
Maggie Dreyer <mdreyer@lclark.edu>
2015-04-16 13:36:25 +0200

    Can upload file via command line

--- src/main/java/main/CloudStorage.java
+++ src/main/java/main/CloudStorage.java
@@ -1,34 +1,80 @@
 package main;
 
 import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.net.URISyntaxException;
+import java.security.InvalidKeyException;
+
+import com.microsoft.azure.storage.CloudStorageAccount;
+import com.microsoft.azure.storage.StorageException;
+import com.microsoft.azure.storage.blob.CloudBlobClient;
+import com.microsoft.azure.storage.blob.CloudBlobContainer;
+import com.microsoft.azure.storage.blob.CloudBlockBlob;
 
 public class CloudStorage {
 
+	public static final String CONNECTION = "DefaultEndpointsProtocol=http;"
+			+ "AccountName=ait1;"
+			+ "AccountKey=UzbdlqbrAIls5IJB1PpJLj1COmw28CP6Pcb7Wlm9JNY+Oo/fgWmwEBMKToX+85r5Rf6pBYKcf2TR9cf+nVieLw==";
+
 	private static final String HELP_TEXT = "Available commands:\n"
-			+ "add filename -- add the specified file to the cloud\n"
-			+ "remove filename -- remove the specified file from the cloud\n"
+			+ "add source_path name -- add the specified file to the cloud as \"name\"\n"
+			+ "download name destination_path -- download the specified file from the cloud and save it at destination_path"
+			+ "remove name -- remove the specified file from the cloud\n"
 			+ "list -- list all the files currently uploaded";
+
+	private static final String ADD = "add";
+
+	private CloudStorageAccount storageAccount;
+	private CloudBlobContainer container;
 
 	public static void main(String[] args) {
 
-		new CloudStorage().processCommands();
+		try {
+			new CloudStorage().processCommands();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 
 	}
 
-	private void processCommands() {
+	public CloudStorage() throws InvalidKeyException, URISyntaxException,
+			StorageException {
+		// Retrieve storage account from connection-string.
+		storageAccount = CloudStorageAccount.parse(CONNECTION);
+
+		// Create the blob client.
+		CloudBlobClient blobClient = storageAccount.createCloudBlobClient();
+
+		// Get a reference to a container.
+		// The container name must be lower case
+		container = blobClient.getContainerReference("mycontainer");
+
+		// Create the container if it does not exist.
+		container.createIfNotExists();
+	}
+
+	private void processCommands() throws IOException {
 		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
 
-		String command = "";
-		try {
-			command = in.readLine();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
+		String command = in.readLine();
 		while (command != "exit") {
-			if (command.startsWith("add")) {
-				// add the specified file to the default blob/container
+			if (command.startsWith(ADD)) {
+				String args = command.substring(ADD.length() + 1);
+				String filePath = args.substring(0, args.indexOf(' '));
+				String destName = args.substring(args.indexOf(' '));
+				try {
+					CloudBlockBlob blob = container
+							.getBlockBlobReference(destName);
+					File source = new File(filePath);
+					blob.upload(new FileInputStream(source), source.length());
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+				System.out.println("File uploaded to cloud as " + destName);
 			} else if (command.startsWith("list")) {
 				// list the files uploaded to the default blob/container
 			} else if (command.startsWith("remove")) {
@@ -38,6 +84,8 @@ public class CloudStorage {
 			} else {
 				System.out.println("Unsupported command!");
 			}
+
+			command = in.readLine();
 		}
 	}
 
